cff2a0690f9b291b560be4f1ddff587156edb1a0
Arinze-Obieze <xboss000000@gmail.com>
2023-06-14 20:04:12 +0100

    designed the landing page, fetched data from the api

---
+++ components/Navbar.js
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Navbar = () => {
+    return (
+        <div>
+
+            <header className="flex shadow-md justify-between mr-1">
+                <span>
+                    <Image src={'/logo.jpg'} width={77} height={44} alt='logo' />
+
+                </span>
+                <Link href={'/'} className='space-x-4'><h1 className="mt-5 text-blue-500 font-serif font-semibold text-lg"> <span className="text-red-500">ABSTRACT</span>    CRYPTO</h1></Link>
+                <nav className="flex mt-5 space-x-4">
+                    <Link className="hover:text-red-500 hover:underline" href={'/'}>Home</Link>
+                    <Link className="hover:text-red-500 hover:underline" href={'/news'}>News</Link>
+                    <Link className="hover:text-red-500 hover:underline" href={'#'} >Chart</Link>
+
+                </nav>
+            </header>
+
+
+        </div>
+    );
+}
+
+export default Navbar;
