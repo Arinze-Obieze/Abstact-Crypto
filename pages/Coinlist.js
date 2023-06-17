c293a338539227ec17657ce867295efba27f557a
Arinze-Obieze <xboss000000@gmail.com>
2023-06-17 02:33:48 +0100

    made the prices to chage colour based on their price and removed exccesive digits from the price

--- pages/Coinlist.js
+++ pages/Coinlist.js
@@ -1,3 +1,4 @@
+import { BiTrendingDown, BiTrendingUp } from "react-icons/bi";
 import Image from "next/image"
 import Link from "next/link"
 import { useEffect, useState } from "react"
@@ -5,7 +6,9 @@ import { useEffect, useState } from "react"
 const Coinlist = () => {
     const [coinlist, setCoinlist] = useState([])
 
-
+    const [perpage, setPerpage] = useState(100)
+    const [page, setPage] =  useState(1)
+const [pricchange,setPricechange]=useState(`24`)
 
     const Fetchcoins = async () => {
         const url = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h&locale=en`
@@ -33,10 +36,12 @@ const Coinlist = () => {
             <nav className="p-5 border-4 space-x-5 font-semibold 
 font-serif text-2xl text-red-500 flex 
 justify-center mt-4">
-                <h4 className="text-black text-lg font-sans font-md">Sort by</h4>
-                <Link href={'#'} className='hover:underline'>Trending</Link>
-                <Link href={'#'} className='hover:underline'>Market Cap</Link>
 
+                <select>
+                    <option value="">Trending</option>
+                    <option value="">Market Cap</option>
+
+                </select>
 
             </nav>
 
@@ -44,18 +49,18 @@ justify-center mt-4">
 
                 {coinlist && coinlist.map((coin) => (
                     <div key={coin.id} >
-                        <div className='flex ml-2 mr-1 p-4 border-2 '>
-                            <h1
-                                className="mr-2">{coin.market_cap_rank}.</h1>
+                        <div className='flex ml-2 mr-1 p-4 border-2 mt-5 shadow-md'>                        <h1
+                            className="mr-2">{coin.market_cap_rank}.</h1>
                             <Image
-                                className="mr-2" width={25} height={35} src={coin.image} alt='coin_image' />
-                            <span className='' >{coin.name}</span>
+                                className="mr-2" width={40} height={30} src={coin.image} alt='coin_image' />
+                            <span className='flex flex-grow' >{coin.name}</span>
                             <div className="flex  justify-between  w-full ">
-
                                 <h1 className="text-gray-400">({coin.symbol})</h1>
-                                <hi>{coin.current_price}</hi>
-                                <h1>{coin.market_cap_change_percentage_24h}</h1>
-                            </div>
+
+                                {coin.market_cap_change_percentage_24h.toString().substring(0, 3) >= 0 ?
+                                    <div className="text-green-500 flex gap-1">{coin.market_cap_change_percentage_24h}%<BiTrendingUp/></div> :
+                                    <div className="text-red-500 flex gap-1">{coin.market_cap_change_percentage_24h}%<BiTrendingDown/></div>}
+                                <hi className='text-gray-700'>${coin.current_price}</hi>                          </div>
 
                         </div>
                     </div>
